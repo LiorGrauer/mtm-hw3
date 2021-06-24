a06c3cbb2f8c0eb693bb8b1796529e3264858c35
Tom R <tomrahav500@gmail.com>
2021-06-24 15:23:04 +0300

    Part 2 ready

--- gradesCalc.py
+++ gradesCalc.py
@@ -12,6 +12,27 @@ def final_grade(input_path: str, output_path: str) -> int:
 # check_strings: Checks if `s1` can be constructed from `s2`'s characters.
 #   s1: The string that we want to check if it can be constructed
 #   s2: The string that we want to construct s1 from
+# consts declaration
+
+ABC_SIZE = 26
+FIRST_LOWER_CASE_LETTER = 'a'
+FIRST_UPPER_CASE_LETTER = 'A'
+LAST_UPPER_CASE_LETTER = 'Z'
+
+def wordToHistCaseInsensitive(str: str) -> list:
+    hist = [0]*ABC_SIZE
+    for letter in str:
+        if letter <=LAST_UPPER_CASE_LETTER and letter >= FIRST_UPPER_CASE_LETTER:
+            hist[ord(letter) - ord(FIRST_UPPER_CASE_LETTER)]+=1
+        else:
+            hist[ord(letter) - ord(FIRST_LOWER_CASE_LETTER)]+=1
+    return hist
+
 def check_strings(s1: str, s2: str) -> bool:
-    # TODO: implement here
+    hist_s1 = wordToHistCaseInsensitive(s1) 
+    hist_s2 = wordToHistCaseInsensitive(s2)  
+    for letter_count_1, letter_count_2 in zip(hist_s1,hist_s2):
+        if(letter_count_1 > letter_count_2):
+            return False
+    return True
     raise NotImplementedError
--- test.py
+++ test.py
@@ -3,18 +3,36 @@ from gradesCalc import *
 
 # Testing your implemented functions, feel free to add more tests below
 def main():
-    # Testing the `final_grade` function
-    input_path = 'tests/input'
-    output_path = 'tests/out'
-    course_avg = final_grade(input_path=input_path, output_path=output_path)
-    assert course_avg == 70
 
-    # Testing the `check_strings` function
+     # Testing the `check_strings` function
     s1 = 'naanb'
     s2 = 'baNaNa'
     result = check_strings(s1=s1, s2=s2)
     assert result
 
+    s3 = 'naanb'
+    s4 = 'baNa'
+    result = check_strings(s3, s4)
+    assert not(result)
+
+    s5 = 'aGGro'
+    s6 = 'gaordg'
+    result = check_strings(s5, s6)
+    assert result
+
+    s7 = 'ananas'
+    result = check_strings(s7, s2)
+    assert not(result)
+
+    s8 = 'bannn'
+    result = check_strings(s8, s2)
+    assert not(result)
+
+    # Testing the `final_grade` function
+    input_path = 'tests/input'
+    output_path = 'tests/out'
+    course_avg = final_grade(input_path=input_path, output_path=output_path)
+    assert course_avg == 70
 
 if __name__ == "__main__":
     main()
