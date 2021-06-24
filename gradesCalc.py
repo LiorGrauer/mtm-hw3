3ca5f1207eff3b0ee1a1f2b66900da672d8b5e70
LiorGrauer <84275385+LiorGrauer@users.noreply.github.com>
2021-06-24 15:23:34 +0300

    part 1

--- gradesCalc.py
+++ gradesCalc.py
@@ -4,9 +4,44 @@
 #   input_path: Path to the file that contains the input
 #   output_path: Path to the file that will contain the output
 def final_grade(input_path: str, output_path: str) -> int:
-    # TODO: implement here
-    raise NotImplementedError
+    input = open(input_path,"r")
+    output = open(output_path,"w")
+    output_list = []
+    for line in input:
+        current_student = line.lstrip().split(',')
+        if (current_student[0][0] != 0 and legalName(current_student[1]) and current_student[2] > 0 and 
+            current_student[3] > 50 and current_student[3] <= 100):
+            int location_of_student = deepContains(output_list, current_student[0])
+            if (location_of_student >= 0):
+                output_list[location_of_student] = current_student 
+            else:
+                output_list += current_student
+    sorted_output_list = sorted(output_list, key=operator.itemgetter(0))
+    for student in sorted_output_list:
+        student_output = [student[0], student[3], calculateFinalGrade(student[0], student[3])]
+    for student in sorted_output_list:
+        output.write(student)
+    input.close()
+    output.close()
 
+def legalName(name: str) -> bool:
+    for letter in name:
+        if !(letter >= 'a' and leter <= 'z') and !(letter >= 'A' and leter <= 'Z'):
+            return false
+    return true
+
+def deepContains(list_of_lists: list, number: int) -> int:
+    for index in range(len(list_of_lists)):
+        for value in list[index]:
+            if value == number:
+                return index
+    return -1
+
+# def returnFirstElement(list: list) -> int
+#     return list[0]
+
+def calculateFinalGrade(id: list, grade: int) -> int:
+    return ((id % 100) + grade) // 2
 
 #### PART 2 ####
 # check_strings: Checks if `s1` can be constructed from `s2`'s characters.
